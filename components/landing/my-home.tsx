66f906589132a962feb93ddc90c59dd2cdc68089
nero19960329 <wangzhao2013tsinghua@gmail.com>
2023-08-08 15:08:10 +0800

    feat: home page add about

--- components/landing/my-home.tsx
+++ components/landing/my-home.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import PreviewLink from "../misc/preview-link";
 
 const MyHome = () => {
   const [isIframeHidden, setIsIframeHidden] = useState(true);
@@ -31,7 +32,7 @@ const MyHome = () => {
           {/* Section header */}
           <div className="text-center">
             <h1 className="text-7xl md:text-8xl font-extrabold leading-tight mb-4" data-aos="zoom-y-out">
-              Wang Zhao's Blog
+              <PreviewLink href="/About">[[Wang Zhao]]</PreviewLink>'s Blog
             </h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
