79cffa5f9860d42f7c23a43c0d7c208b6561d8e9
Whitman Schorn <whitman.schorn@gmail.com>
2023-10-11 09:28:45 -0500

    update font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body class="font-serif">
         <Main />
         <NextScript />
       </body>
