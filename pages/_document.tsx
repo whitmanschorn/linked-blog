90527eea305dbf5f1ab539f6c8cea84ddabdda25
Whitman Schorn <whitman.schorn@gmail.com>
2023-10-11 11:47:44 -0500

    fix font thing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body class="font-serif">
+      <body className="font-serif">
         <Main />
         <NextScript />
       </body>
